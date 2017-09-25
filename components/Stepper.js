6c893bc9ea70951ca147f812672769b76c4cc6a0
Ryan C. Collins <rccproductions@mac.com>
2017-09-25 11:50:27 -0500

    Feat: add Stepper styling

--- components/Stepper.js
+++ components/Stepper.js
@@ -1,25 +1,48 @@
 // @flow
 import React from 'react'
-import { View, TouchableOpacity } from 'react-native'
+import { View, TouchableOpacity, StyleSheet } from 'react-native'
 import { FontAwesome, Entypo } from '@expo/vector-icons'
-import { black } from '../utils/colors'
+import { black, blue } from '../utils/colors'
 
 type Props = {
   onDecrement: () => void,
   onIncrement: () => void,
 }
 
+const styles = StyleSheet.create({
+  view: {
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    borderColor: blue,
+    borderWidth: 1,
+    borderRadius: 3,
+    margin: 10,
+  },
+  button: {
+    flex: 1,
+    display: 'flex',
+    alignItems: 'center',
+  },
+  line: {
+    width: 1,
+    height: '100%',
+    backgroundColor: blue,
+  }
+})
+
 export default function Stepper({
   onDecrement,
   onIncrement,
 }: Props) {
   return (
-    <View>
-      <TouchableOpacity onPress={onDecrement}>
-        <FontAwesome name="minus" size={30} color={black} />
+    <View style={styles.view}>
+      <TouchableOpacity style={styles.button} onPress={onDecrement}>
+        <FontAwesome name="minus" size={20} color={blue} />
       </TouchableOpacity>
-      <TouchableOpacity onPress={onIncrement}>
-        <Entypo name="plus" size={30} color={black} />
+      <View style={styles.line} />
+      <TouchableOpacity style={styles.button} onPress={onIncrement}>
+        <Entypo name="plus" size={30} color={blue} />
       </TouchableOpacity>
     </View>
   )
