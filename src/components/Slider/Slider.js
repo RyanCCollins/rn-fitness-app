a3e22369c68fc1ee7ac4d96dbc7b9cf946f00e81
Ryan Collins <rccproductions@mac.com>
2017-09-28 09:21:02 -0400

    Fix: merge conflict issues

--- src/components/Slider/Slider.js
+++ src/components/Slider/Slider.js
@@ -2,19 +2,8 @@
 
 import React from 'react'
 import { withState, compose } from 'recompose'
-<<<<<<< HEAD:src/components/Slider/Slider.js
 import { View, Text, Slider as RNSlider } from 'react-native'
 import SliderContainer from './SliderContainer'
-=======
-import { View, Text, Slider as RNSlider, StyleSheet } from 'react-native'
-
-const styles = StyleSheet.create({
-  container: {
-    marginLeft: 20,
-    marginRight: 20,
-  },
-})
->>>>>>> d514072cc1dd6c0a3fc9656ec83d539db9201dc3:components/Slider.js
 
 type Props = {
   max: number,
@@ -26,11 +15,7 @@ type Props = {
 
 function Slider({ max, unit, step, value, setValue }: Props): React.Element<*> {
   return (
-<<<<<<< HEAD:src/components/Slider/Slider.js
     <SliderContainer>
-=======
-    <View style={styles.container}>
->>>>>>> d514072cc1dd6c0a3fc9656ec83d539db9201dc3:components/Slider.js
       <RNSlider
         step={step}
         value={value}
--- src/components/TextButton.js
+++ src/components/TextButton.js
@@ -5,21 +5,13 @@ import { Text, TouchableOpacity } from 'react-native'
 
 type Props = {
   onPress: () => void,
-<<<<<<< HEAD:src/components/TextButton.js
   children: Element<*>,
-=======
-  children: React.Node,
->>>>>>> d514072cc1dd6c0a3fc9656ec83d539db9201dc3:components/TextButton.js
 }
 
 export default function TextButton({
   children,
   onPress,
-<<<<<<< HEAD:src/components/TextButton.js
 }: Props) {
-=======
-}: Props): React.Element<*> {
->>>>>>> d514072cc1dd6c0a3fc9656ec83d539db9201dc3:components/TextButton.js
   return (
     <TouchableOpacity onPress={onPress}>
       <Text>{children}</Text>
