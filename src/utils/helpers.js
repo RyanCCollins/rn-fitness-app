d7aa9df2d454d76dbc69bd9b0cb9aca45f40dc72
Ryan Collins <RyanCCollins@users.noreply.github.com>
2017-10-06 19:58:02 -0400

    Update helpers.js

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -5,13 +5,13 @@ import { white, red, orange, blue, lightPurp, pink } from './colors'
 
 const styles = StyleSheet.create({
   iconContainer: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginRight: 20,
     padding: 5,
     borderRadius: 8,
     width: 50,
     height: 50,
-    justifyContent: 'center',
-    alignItems: 'center',
-    marginRight: 20,
   },
 })
 
