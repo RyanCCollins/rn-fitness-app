e8ea067a7f84fe069b6b1f8e2bb52c9e4f80350e
Ryan Collins <RyanCCollins@users.noreply.github.com>
2017-10-12 01:11:00 -0400

    Update History.js

--- src/screens/History/History.js
+++ src/screens/History/History.js
@@ -1,4 +1,10 @@
 import React from 'react'
 import { Text, View } from 'react-native'
 
-export default () => <View><Text>History</Text></View>
+export default () => (
+  <View>
+    <Text>
+      History
+    </Text>
+  </View>
+)
